2e18635e2b75d30ffd2ca3e5eca5452ffe955fba
BM-Daniel <danbabereyir@gmail.com>
2023-09-04 12:46:02 +0000

    Fix issues

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -7,4 +7,5 @@ str2 as arguments and returns a concatenated string
 
 
 def concat(str1: str, str2: str) -> str:
+    '''Type annotated function'''
     return str1 + str2
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,12 @@
 from typing import List
 
 
-# Write a type-annotated function sum_list which takes a list input_list of
-# floats as argument and returns their sum as a float.
+'''
+Write a type-annotated function sum_list which takes a list input_list of
+floats as argument and returns their sum as a float.
+'''
+
 
 def sum_list(input_list: List[float]) -> float:
+    '''Type annotated function'''
     return sum(input_list)
